081634a677875e7ee2b0ca8267879934e68880d7
zajkos <65664846+zajkos@users.noreply.github.com>
2020-11-12 22:12:42 +0100

    Update Config.h

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -56,11 +56,22 @@ public:
         float fov{ 0.0f };
         float smooth{ 1.0f };
         int bone{ 0 };
+        //-->
+        float recoilControlX{ 0.0f };
+        float recoilControlY{ 0.0f };
+        //<--
         float maxAimInaccuracy{ 1.0f };
         float maxShotInaccuracy{ 1.0f };
+        //-->
+        int shotsFired{ 0 };
+        //<--
         int minDamage{ 1 };
         bool killshot{ false };
         bool betweenShots{ true };
+        //-->
+        bool standaloneRCS{ false };
+        bool randomRCS{ true }
+        //<--
     };
     std::array<Aimbot, 40> aimbot;
 
