b9e069a1174a797b5a08739d322b45b2adb1defa
zajkos <65664846+zajkos@users.noreply.github.com>
2020-11-12 21:25:50 +0100

    Update Config.cpp

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -578,6 +578,9 @@ static void from_json(const json& j, Config::Misc& m)
     read(j, "Fast Stop", m.fastStop);
     read<value_t::object>(j, "Bomb timer", m.bombTimer);
     read(j, "Quick reload", m.quickReload);
+    //-->
+    read(j, "Bomb damage indicator", m.bombDamageIndicator);
+    //<-
     read(j, "Prepare revolver", m.prepareRevolver);
     read(j, "Prepare revolver key", m.prepareRevolverKey);
     read(j, "Hit sound", m.hitSound);
@@ -976,6 +979,9 @@ static void to_json(json& j, const Config::Misc& o)
     WRITE("Fast Stop", fastStop);
     WRITE("Bomb timer", bombTimer);
     WRITE("Quick reload", quickReload);
+    //-->
+    WRITE("Bomb damage indicator", bombDamageIndicator);
+    //<--
     WRITE("Prepare revolver", prepareRevolver);
     WRITE("Prepare revolver key", prepareRevolverKey);
     WRITE("Hit sound", hitSound);
